7313ce0a1053c85cd8773ad0839630c601cc0f88
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-08-03 20:26:31 +0700

    footer

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="footer pt-10 text-blue-100 text-sm relative z-50">
+  <div class="footer bg-gradient-to-tr from-yellow-500 to-primary pt-10 text-gray-800 text-sm relative z-50">
     <div class="container relative z-40">
-      <div class="flex justify-between items-center flex-wrap gap-6 lg:gap-10">
+      <div class="flex justify-between flex-wrap gap-6 lg:gap-20">
 
         <div class="flex flex-col gap-4"> <!-- coloumn one -->
         
@@ -16,28 +16,31 @@
           
         <div class="flex gap-3 items-center">
           <div v-for="(i, index) in socmed" :key="index">
-          <a :href="i.link" target="_blank" class="cursor-pointer w-9 h-9 flex items-center justify-center rounded-full bg-gray-700 text-primary hover:text-black hover:bg-primary  duration-300">
+          <a :href="i.link" target="_blank" class="cursor-pointer w-9 h-9 flex items-center justify-center rounded-full bg-black text-primary hover:text-black hover:bg-primary  duration-300">
             <icon :name="i.icon" class=" text-lg " />
           </a>
           </div>
         </div>
         
-    
+        </div>
+        <div>
+         <img src="/logo.png" alt="" class="w-[230px] h-auto object-contain" />
+
+          <img src="/img/satu-indonesia.png" alt="" class="w-[130px] h-auto object-contain">
         </div>
 
-          <img src="/logo.png" alt="" class="w-[230px] h-auto object-contain" />
-
-          <img src="/moving-as-one.png" alt="">
-          <img src="/img/satu-indonesia.png" alt="" class="w-[130px] h-auto object-contain">
+         <div class="flex max-w-[500px] ">
+          <img src="/img/yellow-map.png" alt="">
+         </div>
 
       </div>
     </div>
-    <div class="py-2 mt-10 bg-gray-950 text-gray-500 text-center text-sm border-t border-gray-800">
-      <div class="container text-xs">
+    <div class="py-2 mt-10 bg-gradient-to-tr from-gray-900 via-black to-gray-950 text-gray-500 text-center text-sm border-t border-gray-600">
+      <div class="container text-xs flex flex-col gap-2 items-end">
         <p>© 2025 United Tractors all right reserved.</p>
               <ul class="flex items-center justify-center gap-4">
-           <li><nuxt-link to="/privacy-policy" class="hover:text-primary duration-300">Privacy Policy</nuxt-link></li>
-            <li><nuxt-link to="/privacy-policy" class="hover:text-primary duration-300">Kontak</nuxt-link></li>
+           <li><nuxt-link to="/privacy-policy" class="hover:text-primary duration-300 underline">Privacy Policy</nuxt-link></li>
+            <li><nuxt-link to="/privacy-policy" class="hover:text-primary duration-300 underline">Kontak</nuxt-link></li>
            </ul>
       </div>
     </div>
@@ -71,21 +74,22 @@ const socmed = [
 </script>
 
 <style scoped>
-.footer{
+/* .footer{
   position: relative;
   background: url('/img/bg-2.jpg') repeat-x center center / cover ;
-}
-.footer::after{
+} */
+/* .footer::after{
   content: '';
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   width: 100%;
-  max-width: 400px;
+  max-width: 500px;
   max-height: 260px;
   height: 100%;
-  background: url('/img/map.png') no-repeat center right / contain;
+  background: url('/img/yellow-map.png') no-repeat center right / contain;
   z-index: 0;
-}
+  
+} */
 </style>
